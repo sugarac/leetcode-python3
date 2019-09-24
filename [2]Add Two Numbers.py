506dce515690d7dc381775db925379a6ea4cee50
Xu Wang <uscwangxu@gmail.com>
2019-09-23 22:28:20 -0700

    test private repo

--- [2]Add Two Numbers.py
+++ [2]Add Two Numbers.py
@@ -15,10 +15,10 @@
 
 #leetcode submit region begin(Prohibit modification and deletion)
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
