ae4cb593a84bdcbff5576ef5f773c2c4468ad4b4
Xu Wang <xu@oben.com>
2019-09-23 22:04:04 -0700

    init

---
+++ [1]Two Sum.py
@@ -0,0 +1,25 @@
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target. 
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice. 
+#
+# Example: 
+#
+# 
+#Given nums = [2, 7, 11, 15], target = 9,
+#
+#Because nums[0] + nums[1] = 2 + 7 = 9,
+#return [0, 1].
+#
+# Related Topics Array Hash Table
+
+
+
+#leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        
+#leetcode submit region end(Prohibit modification and deletion)
